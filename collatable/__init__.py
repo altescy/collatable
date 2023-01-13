e00dbfdb731ce08955cfaab220365f6aae83b9e5
altescy <altescy@fastmail.com>
2023-01-14 04:38:45 +0900

    imports

--- collatable/__init__.py
+++ collatable/__init__.py
@@ -1,3 +1,27 @@
 from importlib.metadata import version
 
+from collatable.fields import (  # noqa: F401
+    Field,
+    IndexField,
+    LabelField,
+    ListField,
+    ScalarField,
+    SequenceField,
+    TensorField,
+    TextField,
+)
+from collatable.instance import Instance, collate  # noqa: F401
+
 __version__ = version("collatable")
+__all__ = [
+    "Field",
+    "IndexField",
+    "LabelField",
+    "ListField",
+    "ScalarField",
+    "SequenceField",
+    "TensorField",
+    "TextField",
+    "Instance",
+    "collate",
+]
---
+++ collatable/fields/__init__.py
@@ -0,0 +1,8 @@
+from collatable.fields.field import Field  # noqa: F401
+from collatable.fields.index_field import IndexField  # noqa: F401
+from collatable.fields.label_field import LabelField  # noqa: F401
+from collatable.fields.list_field import ListField  # noqa: F401
+from collatable.fields.scalar_field import ScalarField  # noqa: F401
+from collatable.fields.sequence_field import SequenceField  # noqa: F401
+from collatable.fields.tensor_field import TensorField  # noqa: F401
+from collatable.fields.text_field import TextField  # noqa: F401
