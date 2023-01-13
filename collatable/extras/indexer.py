3934b0bc5672cacbe3c51571eab0e0d3bdc7366f
altescy <altescy@fastmail.com>
2023-01-14 03:29:38 +0900

    add default/spetial value option

--- collatable/extras/indexer.py
+++ collatable/extras/indexer.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from typing import Dict, Generic, Hashable, Iterator, List, Sequence, TypeVar
+from typing import Dict, Generic, Hashable, Iterator, List, Optional, Sequence, TypeVar
 
 import numpy
 
@@ -10,9 +10,15 @@ Self = TypeVar("Self", bound="Indexer")
 
 
 class Indexer(Generic[T_Value]):
-    def __init__(self) -> None:
-        self._index_to_value: List[T_Value] = []
-        self._value_to_index: Dict[T_Value, int] = {}
+    def __init__(
+        self,
+        *,
+        spetials: Sequence[T_Value] = (),
+        default: Optional[T_Value] = None,
+    ) -> None:
+        self._index_to_value: List[T_Value] = list(spetials)
+        self._value_to_index: Dict[T_Value, int] = {value: index for index, value in enumerate(spetials)}
+        self._default_value = default
         self._train = False
 
     def __len__(self) -> int:
@@ -28,7 +34,12 @@ class Indexer(Generic[T_Value]):
         self._train = False
 
     def get_value_by_index(self, index: int) -> T_Value:
-        return self._index_to_value[index]
+        try:
+            return self._index_to_value[index]
+        except IndexError:
+            if self._default_value is not None:
+                return self._default_value
+            raise
 
     def get_index_by_value(self, value: T_Value) -> int:
         if value not in self._value_to_index:
