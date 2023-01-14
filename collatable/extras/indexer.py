14095d5ec09877e8992c4327b2122f77c2e453d3
altescy <altescy@fastmail.com>
2023-01-14 09:41:36 +0900

    add Indexer.from_* methods

--- collatable/extras/indexer.py
+++ collatable/extras/indexer.py
@@ -1,5 +1,19 @@
 from contextlib import contextmanager
-from typing import Dict, Generic, Hashable, Iterable, Iterator, List, Optional, Sequence, Set, TypeVar
+from typing import (
+    Dict,
+    Generic,
+    Hashable,
+    Iterable,
+    Iterator,
+    List,
+    Mapping,
+    Optional,
+    Sequence,
+    Set,
+    TypeVar,
+    Union,
+    cast,
+)
 
 import numpy
 
@@ -68,6 +82,73 @@ class Indexer(Generic[T_Value]):
             self._value_to_index[value] = len(self._index_to_value)
             self._index_to_value.append(value)
         return self._value_to_index[value]
+
+    @classmethod
+    def from_iterable(
+        cls,
+        iterable: Iterable[T_Value],
+        *,
+        ignores: Iterable[T_Value] = (),
+        specials: Sequence[T_Value] = (),
+        default: Optional[T_Value] = None,
+    ) -> "Indexer[T_Value]":
+        indexer = cls(ignores=ignores, specials=specials, default=default)
+        for value in iterable:
+            indexer[value]
+        return indexer
+
+    @classmethod
+    def from_vocab(cls, vocab: Mapping[T_Value, int]) -> "Indexer[T_Value]":
+        class PlaceHolder:
+            ...
+
+        placeholder = PlaceHolder()
+
+        indexer: Indexer[T_Value] = cls()
+        index_to_value: List[Union[T_Value, PlaceHolder]] = [placeholder] * len(vocab)
+        value_to_index: Dict[T_Value, int] = {}
+        for value, index in vocab.items():
+            if index < 0:
+                raise ValueError("index must be non-negative")
+            if index >= len(index_to_value):
+                raise ValueError("index must be less than the length of the vocabulary")
+            if not isinstance(index_to_value[index], PlaceHolder):
+                raise ValueError("index must be unique")
+            index_to_value[index] = value
+            value_to_index[value] = index
+
+        indexer._index_to_value = cast(List[T_Value], index_to_value)
+        indexer._value_to_index = value_to_index
+        return indexer
+
+    @classmethod
+    def from_documents(
+        cls,
+        documents: Iterable[Sequence[T_Value]],
+        *,
+        min_df: Union[int, float] = 1,
+        max_df: Union[int, float] = 1.0,
+        ignores: Iterable[T_Value] = (),
+        specials: Sequence[T_Value] = (),
+        default: Optional[T_Value] = None,
+    ) -> "Indexer[T_Value]":
+        num_documents = 0
+        value_to_df: Dict[T_Value, int] = {}
+        for tokens in documents:
+            num_documents += 1
+            for token in tokens:
+                value_to_df[token] = value_to_df.get(token, 0) + 1
+
+        min_df = int(min_df) if isinstance(min_df, int) else int(min_df * num_documents)
+        max_df = int(max_df) if isinstance(max_df, int) else int(max_df * num_documents)
+
+        indexer = cls(ignores=ignores, specials=specials, default=default)
+
+        for token, df in list(value_to_df.items()):
+            if min_df <= df <= max_df:
+                indexer[token]
+
+        return indexer
 
 
 class TokenIndexer(Indexer[str]):
