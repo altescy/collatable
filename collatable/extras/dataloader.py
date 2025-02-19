c0b4b1fca85ed516403b87a28f97658b074f66ae
altescy <altescy@fastmail.com>
2025-02-19 15:57:23 +0900

    add batch sampler

--- collatable/extras/dataloader.py
+++ collatable/extras/dataloader.py
@@ -1,66 +1,87 @@
 import math
 import random
-from typing import Dict, Iterator, Mapping, Optional, Sequence
+from typing import Dict, Iterable, Iterator, Mapping, Optional, Protocol, Sequence, TypeVar
 
 from collatable.collator import Collator
 from collatable.fields import Field
 from collatable.typing import DataArray
 
+T = TypeVar("T")
+T_co = TypeVar("T_co", covariant=True)
 
-class BatchIterator:
+
+class SizedIterator(Protocol[T_co]):
+    def __len__(self) -> int: ...
+
+    def __next__(self) -> T_co: ...
+
+    def __iter__(self) -> Iterator[T_co]: ...
+
+
+class BatchIterator(SizedIterator[Dict[str, DataArray]]):
     def __init__(
         self,
         dataset: Sequence[Mapping[str, Field]],
-        batch_size: int = 1,
-        shuffle: bool = False,
-        drop_last: bool = False,
+        indices: Iterable[Sequence[int]],
+        num_batches: int,
         collator: Optional[Collator] = None,
     ) -> None:
         self._dataset = dataset
-        self._batch_size = batch_size
-        self._shuffle = shuffle
-        self._drop_last = drop_last
-        self._offset = 0
+        self._indices = iter(indices)
+        self._num_batches = num_batches
         self._collator = collator or Collator()
-        self._indices = list(range(len(self._dataset)))
-        if self._shuffle:
-            random.shuffle(self._indices)
 
     def __len__(self) -> int:
-        if self._drop_last:
-            return len(self._dataset) // self._batch_size
-        return math.ceil(len(self._dataset) / self._batch_size)
+        return self._num_batches
 
     def __next__(self) -> Dict[str, DataArray]:
-        if self._offset >= len(self._dataset):
-            raise StopIteration
-        if self._offset + self._batch_size > len(self._dataset):
-            if self._drop_last:
-                raise StopIteration
-            batch_indices = self._indices[self._offset :]
-        else:
-            batch_indices = self._indices[self._offset : self._offset + self._batch_size]
-        self._offset += self._batch_size
-        return self._collator([self._dataset[i] for i in batch_indices])
+        indices = next(self._indices)
+        return self._collator([self._dataset[i] for i in indices])
 
     def __iter__(self) -> Iterator[Dict[str, DataArray]]:
         return self
 
 
-class DataLoader:
+class IBatchSampler(Protocol):
+    def __call__(self, dataset: Sequence) -> SizedIterator[Mapping[str, DataArray]]: ...
+
+
+class DefaultBatchSampler:
     def __init__(
-        self, batch_size: int = 1, shuffle: bool = False, drop_last: bool = False, collator: Optional[Collator] = None
+        self,
+        batch_size: int = 1,
+        shuffle: bool = False,
+        drop_last: bool = False,
     ) -> None:
         self._batch_size = batch_size
         self._shuffle = shuffle
         self._drop_last = drop_last
+
+    def __call__(self, dataset: Sequence) -> BatchIterator:
+        num_batches = (
+            len(dataset) // self._batch_size if self._drop_last else math.ceil(len(dataset) / self._batch_size)
+        )
+        indices = list(range(len(dataset)))
+        if self._shuffle:
+            random.shuffle(indices)
+
+        def iter_batches() -> Iterator[Sequence[int]]:
+            for batch_index in range(num_batches):
+                start_index = batch_index * self._batch_size
+                end_index = start_index + self._batch_size
+                yield indices[start_index:end_index]
+
+        return BatchIterator(dataset, iter_batches(), num_batches)
+
+
+class DataLoader:
+    def __init__(
+        self,
+        sampler: Optional[IBatchSampler] = None,
+        collator: Optional[Collator] = None,
+    ) -> None:
+        self._sampler = sampler or DefaultBatchSampler()
         self._collator = collator or Collator()
 
-    def __call__(self, dataset: Sequence[Mapping[str, Field]]) -> BatchIterator:
-        return BatchIterator(
-            dataset,
-            batch_size=self._batch_size,
-            shuffle=self._shuffle,
-            drop_last=self._drop_last,
-            collator=self._collator,
-        )
+    def __call__(self, dataset: Sequence[Mapping[str, Field]]) -> SizedIterator[Mapping[str, DataArray]]:
+        return self._sampler(dataset)
